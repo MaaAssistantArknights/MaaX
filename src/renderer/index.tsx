d0e6e1cac800f5fc9be7c58cec3977cc8abdd504
_ChingC <chingc.ccmail@gmail.com>
2022-01-07 17:27:29 +0800

    fix: 修复router和Anchor组件冲突

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
 render(
   <React.StrictMode>
-    <HashRouter>
+    <MemoryRouter>
       <App />
-    </HashRouter>
+    </MemoryRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
