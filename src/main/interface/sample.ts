4864f8c5482e5ebe2049d4bc3f3befbff2068cff
ChingCdesu <chingc.ccmail@gmail.com>
2022-01-13 18:07:25 +0800

    style: 分离出依赖库列表

--- src/main/interface/sample.ts
+++ src/main/interface/sample.ts
@@ -16,19 +16,19 @@ export default () => {
   const ax = asst.CreateEx(cb, voidPointer());
   // asst.CatchDefault()
   if (asst.CatchCustom('127.0.0.1:5555')) {
-    console.log('连接成功');
+    console.log('successfully connected');
     // asst.AppendMall(ax,true)
     // asst.AppendVisit(ax)
     // asst.AppendInfrast(ax,1,["Mfg","Trade"],2,"Money",0.3)
     // asst.AppendInfrast(ax,1,["Control"],1,"_NotUse",0.4)
 
-    asst.AppendRecruit(1, [3, 4, 5], 3, [3, 4, 5], 3, true);
-    asst.AppendAward(ax);
+    // asst.AppendRecruit(1, [3, 4, 5], 3, [3, 4, 5], 3, true);
+    // asst.AppendAward(ax);
 
     // asst.StartRecruitCalc(ax,[4,5,6],3,true)
-    asst.Start(ax);
+    // asst.Start(ax);
   } else {
-    console.log('连接失败');
+    console.log('failed to connect');
   }
   asst.Destroy(ax);
 };
