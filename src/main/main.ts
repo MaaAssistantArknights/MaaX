d4b106a3ad16f25641376296daa9c70f5cc60c4a
bakashigure <bakashigure@hotmail.com>
2022-01-20 14:37:48 +0800

    feat: 添加主要逻辑

--- src/main/main.ts
+++ src/main/main.ts
@@ -15,6 +15,7 @@ import { app, BrowserWindow, ipcMain, shell } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import { is } from 'electron-util';
+import type { Type as InfrastructureType } from 'main/storage/configuration/infrastructure';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
@@ -22,7 +23,7 @@ import storage from './storage';
 
 import interfaceTest from './interface/sample';
 import bluestackPort from './interface/bluestack';
-import { Assistant, voidPointer } from './interface';
+import { Assistant, voidPointer, cb } from './interface';
 
 export default class AppUpdater {
   constructor() {
@@ -32,32 +33,110 @@ export default class AppUpdater {
   }
 }
 
+function reply(event: Electron.IpcMainEvent, msg: string) {
+  event.sender.send('asynchronous-reply', msg);
+}
+
 let mainWindow: BrowserWindow | null = null;
 const RESOURCES_PATH = app.isPackaged
   ? path.join(process.resourcesPath, 'assets')
   : path.join(__dirname, '../../assets');
 
 const Asst = Assistant.getInstance(RESOURCES_PATH);
+
 ipcMain.on('linkstart', async (event, arg) => {
   const port = bluestackPort(
     storage?.get('configuration').connection[
       'Filepath of bluestack.conf'
     ] as string
   );
-  Asst.CreateEx(Asst.callback, voidPointer());
+  Asst.CreateEx(cb, voidPointer());
+  let catchRet = false;
   if (port) {
     storage?.set('configuration.connection.address', `127.0.0.1:${port}`);
     const adr = storage?.get('configuration.connection.address') as string;
-    console.log(`custom address: ${adr}`);
-    const ret = Asst.CatchCustom(adr);
-    console.log(ret ? 'connected to emulator' : 'connect failed');
-  } else {
+    console.log(`try catchCustom() on ${adr}`);
+    catchRet = Asst.CatchCustom(adr);
+    console.log(
+      catchRet
+        ? 'connected to emulator'
+        : 'connect custom address failed, try catchDefault()'
+    );
+  }
+  if (!catchRet) {
     const ret = Asst.CatchDefault();
     console.log(ret ? 'connected to emulator' : 'connect failed');
   }
-  Asst.AppendVisit();
-  Asst.AppendAward();
-  Asst.Start();
+  event.returnValue = catchRet;
+});
+
+ipcMain.on('appendTasks', async (event, arg) => {
+  const tasks = storage?.get('task');
+  tasks?.forEach((singleTask) => {
+    if (!singleTask.enabled) {
+      return;
+    }
+    switch (singleTask.value) {
+      case 'awake':
+        Asst.AppendStartUp();
+        break;
+      case 'clear sanity':
+        // TODO 等关卡信息加入存储后再做
+        // Asst.AppendFight();
+        break;
+      case 'auto recruits':
+        {
+          const recruit = storage?.get('configuration').recruitment;
+          const maxTimes = recruit?.MaximumNumberOfRecruitments;
+          // const selectLevel =
+          // TODO 等star变成array
+        }
+        break;
+      case 'shift scheduling':
+        {
+          const infraRecord: Record<string, string> = {
+            ControlCenter: 'Control',
+            Dormitory: 'Dorm',
+            ManufacturingStation: 'Mfg',
+            MeetingRoom: 'Reception',
+            Office: 'Office',
+            PowerStation: 'Power',
+            TradingStation: 'Trade',
+          };
+          const droneUseRecord: Record<string, string> = {
+            None: '_NotUse',
+            LMD: 'Money',
+            Orundum: 'SyntheticJade',
+            'Battle Record': 'CombatRecord',
+            'Pure Gold': 'PureGold',
+            'Originium Shard': 'OriginStone',
+            Chip: 'Chip',
+          };
+          const infra = storage?.get(
+            'configuration.infrastructure'
+          ) as InfrastructureType;
+          const moodLimit = infra.MoodLimit;
+          const order = infra.enable as Record<string, boolean>;
+          // TODO 基建
+          // Asst.AppendInfrast()
+        }
+        break;
+      case 'visit friends':
+        Asst.AppendVisit();
+        break;
+      case 'shopping':
+        {
+          const buy = storage?.get('configuration').mall.enable as boolean;
+          Asst.AppendMall(buy);
+        }
+        break;
+      case 'receive rewards':
+        Asst.AppendAward();
+        break;
+      default:
+        break;
+    }
+  });
 });
 
 if (process.env.NODE_ENV === 'production') {
