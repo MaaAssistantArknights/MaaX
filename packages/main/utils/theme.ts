aa1bd234d1a0d0d449fc346677c388a9cc11e21a
_ChingC <chingc.ccmail@gmail.com>
2022-02-23 00:44:34 +0800

    fix: 修复除了windows以外的平台无法编译通过

--- packages/main/utils/theme.ts
+++ packages/main/utils/theme.ts
@@ -1,18 +1,20 @@
-import { nativeTheme, BrowserWindow } from "electron"
-import { setVibrancy, BrowserWindow as AcrylicBrowserWindow } from "electron-acrylic-window"
-import { is } from "electron-util"
+import { nativeTheme, BrowserWindow } from "electron";
+import { is } from "electron-util";
 
 export default function useTheme(window: BrowserWindow) {
   const event = () => {
-    const isDark = nativeTheme.shouldUseDarkColors
-    window.webContents.send("theme:update", isDark ? "maa-dark" : "maa-light")
+    const isDark = nativeTheme.shouldUseDarkColors;
+    window.webContents.send("theme:update", isDark ? "maa-dark" : "maa-light");
     if (is.windows) {
-      setVibrancy(window as AcrylicBrowserWindow, {
+      const {
+        setVibrancy
+      } = require("electron-acrylic-window");
+      setVibrancy(window, {
         theme: isDark ? "dark" : "light",
         effect: "blur",
-      })
+      });
     }
-  }
-  window.webContents.on("did-finish-load", event)
-  nativeTheme.on("updated", event)
+  };
+  window.webContents.on("did-finish-load", event);
+  nativeTheme.on("updated", event);
 }
