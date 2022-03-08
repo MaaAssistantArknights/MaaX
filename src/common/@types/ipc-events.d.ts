64c24d93596ac500783494ee5f64185d919a27bd
_ChingC <chingc.ccmail@gmail.com>
2022-03-08 11:17:16 +0800

    chore: 删除react部分

--- src/common/@types/ipc-events.d.ts
+++
@@ -1,9 +0,0 @@
-type StorageEvents =
-  | 'storage:get'
-  | 'storage:set'
-  | 'storage:has'
-  | 'storage:error';
-
-type AsstEvents = 'asst:linkstart' | 'asst:appendTasks';
-
-type IPCEvents = StorageEvents | AsstEvents;
--- src/common/@types/task.d.ts
+++
@@ -1,7 +0,0 @@
-type TaskRunningType = {
-  label: string;
-  value: string;
-  status: 'normal' | 'success' | 'exception';
-  progress: number;
-  enabled: boolean;
-};
