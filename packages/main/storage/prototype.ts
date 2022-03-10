d23bbfe4f8a89e09b12b787ce15a0b3c10a828c3
_ChingC <chingc.ccmail@gmail.com>
2022-03-10 08:55:41 +0800

    feat: 完成storage 75%

--- packages/main/storage/prototype.ts
+++ packages/main/storage/prototype.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import { app, shell } from "electron";
 import _ from "lodash";
 import logger from "../utils/logger";
-import { Observable } from "object-observer";
+// import { Observable } from "object-observer";
 
 type StorageOption<T> = Partial<{
   defaults: T;
@@ -17,43 +17,55 @@ type StorageOption<T> = Partial<{
 const convertExt = (str?: string) => (/\.\w+/.test(str || "") ? str : false);
 
 class Storage<T extends Object> {
-  constructor(option: StorageOption<T>) {
-    this.m_storage = option.defaults || Object.create({});
-    this.m_cwd = option.cwd || app.getPath("userData");
-    this.m_ext = convertExt(option.ext) || ".json";
-    this.m_name = option.name || "config";
+  constructor(option?: StorageOption<T>) {
+    this.m_storage = option?.defaults || Object.create({});
+    this.m_cwd = option?.cwd || app.getPath("userData");
+    this.m_ext = convertExt(option?.ext) || ".json";
+    this.m_name = option?.name || "config";
     this.m_serialize =
-      option.serialize || ((value: T) => JSON.stringify(value, null, "\t"));
-    this.m_deserialize = option.deserialize || JSON.parse;
+      option?.serialize || ((value: T) => JSON.stringify(value, null, "\t"));
+    this.m_deserialize = option?.deserialize || JSON.parse;
+
+    if (!fs.existsSync(this.m_cwd)) {
+      fs.mkdirSync(this.m_cwd);
+    }
+
+    if (!fs.existsSync(this.filepath)) {
+      fs.writeFileSync(this.filepath, "");
+    } else {
+      this.readFromFile();
+    }
 
     this.m_watcher = fs.watch(this.filepath, (eventType) => {
       if (eventType === "change") {
-        if (!this.m_updateBySelf) {
-          this.readFromFile();
-        }
-        this.m_updateBySelf = false;
+        console.log("config file changed");
+        this.readFromFile();
       }
     });
+
+    // this.m_observer = Observable.from(this.m_storage);
+    // Observable.observe(this.m_observer, )
   }
 
-  get(key: string) {}
+  get(key: string) {
+    return _.get(this.m_storage, key);
+  }
 
-  set(key: string, value: unknown) {}
+  set(key: string, value: unknown) {
+    _.set(this.m_storage, key, value);
+    this.saveToFile();
+  }
 
-  has(key: string) {}
+  has(key: string) {
+    return _.has(this.m_storage, key);
+  }
 
   public get filepath(): string {
     return path.join(this.m_cwd, this.m_name + this.m_ext);
   }
 
   private saveToFile = _.debounce(() => {
-    fs.writeFile(this.filepath, this.m_serialize(this.m_storage), (error) => {
-      if (error) {
-        logger.error("error while write config file:", this.filepath, error);
-      } else {
-        this.m_updateBySelf = true;
-      }
-    });
+    fs.writeFileSync(this.filepath, this.m_serialize(this.m_storage));
   }, 1000);
 
   private readFromFile = () => {
@@ -75,7 +87,7 @@ class Storage<T extends Object> {
 
   private m_timer: number | null = null;
   private m_watcher: fs.FSWatcher;
-  private m_updateBySelf = false;
+  // private m_observer;
 }
 
 export default Storage;
