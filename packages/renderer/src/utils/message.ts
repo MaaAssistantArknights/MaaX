272e4b8742c86ccb4c94d6384ccb10a6a3e484b1
bakashigure <bakashigure@hotmail.com>
2022-03-27 20:32:43 +0800

    refactor: 全局Message

--- packages/renderer/src/utils/message.ts
+++ packages/renderer/src/utils/message.ts
@@ -1,18 +1,11 @@
-import { MessageReactive, useMessage, MessageOptions } from "naive-ui";
+import {  MessageOptions } from "naive-ui";
 import { VNodeChild } from "vue";
 
-const messageApi = useMessage();
-
-let message: MessageReactive | null = null;
 
 export const hide = () => {
-  if (message) {
-    message.destroy();
-    message = null;
-  }
-};
+  window.$message.destroyAll();};
 
 export const show = (content: string | (() => VNodeChild), options: MessageOptions) => {
   hide();
-  message = messageApi.create(content, options);
+  window.$message.create(content, options);
 };
