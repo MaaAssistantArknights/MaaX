aa1ac373fa4add5c33031d74ec231327f07e11e8
_ChingC <chingc.ccmail@gmail.com>
2022-05-08 02:22:13 +0800

    test: back to axios

--- packages/common/api/ApiService.ts
+++ packages/common/api/ApiService.ts
@@ -1,33 +1,51 @@
 import SuperAgent from 'superagent'
+import axios, { AxiosAdapter, AxiosInstance } from 'axios'
 import pkg from '../../../package.json'
 
-console.log(globalThis.window)
-
 const getCoreVersion = async (): Promise<string> => {
-  if (globalThis.window) {
-    const version = await (window.ipcRenderer.invoke('version:core') as Promise<string>)
-    return version ? `(core v${version})` : '(without core)'
-  } else {
+  if (typeof XMLHttpRequest === 'undefined') {
     const { Assistant } = await import('../../main/coreLoader')
     const version = Assistant.getInstance()?.GetVersion()
+    return version ? `(core v${version})` : '(without core)'
+  } else {
+    const version = await (window.ipcRenderer.invoke('version:core') as Promise<string>)
     return version ? `(core v${version})` : '(without core)'
   }
 }
 
 const getUiVersion = (): string => pkg.version
 
+const getAdapter = (): AxiosAdapter => {
+  if (typeof XMLHttpRequest === 'undefined') {
+    return require('axios/lib/adapters/http')
+  } else {
+    return require('axios/lib/adapters/xhr')
+  }
+}
+
 class ApiService {
   constructor (baseUrl?: string) {
     this._baseUrl = baseUrl
+    this._instance = axios.create({
+      baseURL: baseUrl,
+      timeout: 5000,
+      adapter: getAdapter()
+    })
   }
 
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   async get<T>(url: string, params: Record<string, any> = {}): Promise<T | Error> {
     try {
-      const response = await SuperAgent
-        .get(`${this._baseUrl ?? ''}${url}`)
-        .query(params)
-        .set('Client-Version', `MeoAssistantArknights v${getUiVersion()} ${await getCoreVersion()}`)
+      const response = this._instance.get(`${this._baseUrl ?? ''}${url}`, {
+        headers: {
+          'Client-Version': `MeoAssistantArknights v${getUiVersion()} ${await getCoreVersion()}`
+        },
+        params
+      })
+      // const response = await SuperAgent
+      //   .get()
+      //   .query(params)
+      //   .set('Client-Version', `MeoAssistantArknights v${getUiVersion()} ${await getCoreVersion()}`)
       return response.body as T
     } catch (error) {
       return new Error(error)
@@ -39,6 +57,7 @@ class ApiService {
   }
 
   private readonly _baseUrl: string | undefined
+  private readonly _instance: AxiosInstance
 }
 
 export default ApiService
