d4bba6b2afe89bd95374f441298064fdb6599d2b
_ChingC <chingc.ccmail@gmail.com>
2022-08-11 23:44:30 +0800

    feat: disable SystemStatus

--- packages/renderer/src/components/Setting/Developer.vue
+++ packages/renderer/src/components/Setting/Developer.vue
@@ -1,13 +1,12 @@
 <script lang="ts" setup>
 import { NSpace } from 'naive-ui'
 import SystemInformation from './SystemInformation.vue'
-import SystemStatus from './SystemStatus.vue'
 </script>
 
 <template>
   <NSpace vertical>
     <SystemInformation />
-    <SystemStatus />
+    <!-- <SystemStatus /> -->
     <!-- <h3 class="subtitle">系统状态</h3>
     <NSkeleton v-if="ssLoading" height="800px" :sharp="false" />
     <JsonViewer v-else :value="ss" copyable :theme="themeStore.theme.replace('maa-', '')" /> -->
