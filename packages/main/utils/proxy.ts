58c6513028741fd94f7a2b2a21e932cca62294f2
_ChingC <chingc.ccmail@gmail.com>
2022-10-22 01:42:18 +0800

    fix: 没有系统代理时，不设置proxy

--- packages/main/utils/proxy.ts
+++ packages/main/utils/proxy.ts
@@ -1,9 +1,10 @@
 import type { BrowserWindow } from 'electron'
 
-async function detectSystemProxy (window: BrowserWindow): Promise<string> {
+async function detectSystemProxy (window: BrowserWindow): Promise<string | undefined> {
   const ses = window.webContents.session
-  const proxy = await ses.resolveProxy('https://www.google.com')
-  return `http://${proxy.replace('PROXY', '').trim()}`
+  const resolved = await ses.resolveProxy('https://www.google.com')
+  const proxy = resolved.replace('PROXY', '').trim()
+  return proxy === 'direct' ? undefined : `http://${proxy}`
 }
 
 export default detectSystemProxy
