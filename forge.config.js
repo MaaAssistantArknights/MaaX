7ae98a0a45afd84b50ce4539e81849b7ed608aa9
_ChingC <chingc.ccmail@gmail.com>
2023-04-21 13:40:02 +0800

    chore: forge打包（还有bug）

---
+++ forge.config.js
@@ -0,0 +1,67 @@
+const info = require('./package.json');
+
+module.exports = {
+  packagerConfig: {
+    icon: 'packages/common/resources/icon',
+    appBundleId: 'com.maa.maa-x',
+    productName: 'MaaX',
+    ignore: (filepath) => {
+      if (filepath.length === 0) {
+        return false;
+      }
+      if (/^\/dist/.test(filepath)) {
+        return false;
+      }
+      if (/^\/package.json/.test(filepath)) {
+        return false;
+      }
+      if (/^\/node_modules/.test(filepath)) {
+        return false;
+      }
+      return true;
+    }
+    // asar: true,
+  },
+  rebuildConfig: {
+    buildPath: __dirname,
+    extraModules: Object.keys(info.dependencies),
+    force: true,
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-vite',
+      config: {
+        // `build` can specify multiple entry builds, which can be
+        // Main process, Preload scripts, Worker process, etc.
+        build: [
+          {
+            // `entry` is an alias for `build.lib.entry`
+            // in the corresponding file of `config`.
+            entry: 'packages/main/index.ts',
+            config: 'packages/main/vite.config.ts',
+          },
+          {
+            entry: 'packages/preload/index.ts',
+            config: 'packages/preload/vite.config.ts',
+          },
+        ],
+        renderer: [
+          {
+            name: 'main_window',
+            config: 'packages/renderer/vite.config.ts',
+          },
+        ],
+      },
+    },
+  ],
+  hooks: {
+    postPackage: async (forgeConfig, options) => {
+      console.info('Packages built at:', options.outputPaths);
+    }
+  }
+};
