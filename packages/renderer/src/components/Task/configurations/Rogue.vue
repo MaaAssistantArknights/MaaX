ba2554907e45de8cb1ad0b93ad49b1a39e63ea09
bakashigure <bakashigure@hotmail.com>
2023-04-23 21:48:19 +0800

    feat: 肉鸽界面更新

--- packages/renderer/src/components/Task/configurations/Rogue.vue
+++ packages/renderer/src/components/Task/configurations/Rogue.vue
@@ -1,38 +1,101 @@
 <script setup lang="ts">
-import { ref, onMounted, Ref, inject } from 'vue'
+import { inject } from 'vue'
 import _ from 'lodash'
+import { RogueTheme } from '@common/enum/core'
 import {
+  NForm,
   NFormItem,
   NSelect,
   NSpace,
-  NInputNumber
+  NInputNumber,
+  NCheckbox,
+  NInput,
+  NTooltip
 } from 'naive-ui'
-import gamedataApi from '@/api/gamedata'
+import { showMessage } from '@/utils/message'
 
 // import logger from '@/hooks/caller/logger'
 
-type squadType = '默认分队' | '指挥分队' | '集群分队'| '后勤分队'| '矛头分队'| '突击战术分队'| '堡垒战术分队'| '远程战术分队' |'破坏战术分队'| '研究分队'| '高规格分队'
-type rolesType = '默认职业组' | '稳扎稳打' | '取长补短' | '随心所欲' | '先手必胜'
+type phantomSquadType = '指挥分队' | '集群分队'| '后勤分队'| '矛头分队'| '突击战术分队'| '堡垒战术分队'| '远程战术分队' |'破坏战术分队'| '研究分队'| '高规格分队'
+type mizukiSquadType = '心胜于物分队' | '物尽其用分队' | '以人为本分队' | '指挥分队' | '集群分队'| '后勤分队'| '矛头分队'| '突击战术分队'| '堡垒战术分队'| '远程战术分队' |'破坏战术分队'| '研究分队'| '高规格分队'
+
+type rolesType = | '稳扎稳打' | '取长补短' | '随心所欲' | '先手必胜'
 
 interface RogueConfiguration {
-  // strategy: Strategies;
-  // operators: Array<OperatorOption>;
+  theme: RogueTheme.Phantom | RogueTheme.Mizuki;
   mode: number;
   starts_count: number,
+  investment_enabled: boolean,
   investments_count: number,
   stop_when_investment_full: boolean,
   squad: string,
   roles: string,
-  core_char: ''
+  core_char: '',
+  use_support: boolean,
+  use_nonfriend_support: boolean
 }
 
-const squadOptions: {
+const phantomSquadOptions: {
   label: string;
-  value: squadType;
+  value: phantomSquadType;
 }[] = [
   {
-    label: '默认分队',
-    value: '默认分队'
+    label: '指挥分队',
+    value: '指挥分队'
+  },
+  {
+    label: '集群分队',
+    value: '集群分队'
+  },
+  {
+    label: '后勤分队',
+    value: '后勤分队'
+  },
+  {
+    label: '矛头分队',
+    value: '矛头分队'
+  },
+  {
+    label: '突击战术分队',
+    value: '突击战术分队'
+  },
+  {
+    label: '堡垒战术分队',
+    value: '堡垒战术分队'
+  },
+  {
+    label: '远程战术分队',
+    value: '远程战术分队'
+  },
+  {
+    label: '破坏战术分队',
+    value: '破坏战术分队'
+  },
+  {
+    label: '研究分队',
+    value: '研究分队'
+  },
+  {
+    label: '高规格分队',
+    value: '高规格分队'
+  }
+]
+
+const mizukiSquadOptions: {
+  label: string;
+  value: mizukiSquadType;
+}[] = [
+  {
+    label: '心胜于物分队',
+    value: '心胜于物分队'
+  },
+  {
+    label: '物尽其用分队',
+    value: '物尽其用分队'
+  },
+  {
+    label: '以人为本分队',
+    value: '以人为本分队'
   },
   {
     label: '指挥分队',
@@ -81,10 +144,6 @@ const rolesOptions: {
   value: rolesType;
 }[] = [
   {
-    label: '默认职业组',
-    value: '默认职业组'
-  },
-  {
     label: '随心所欲(三张随机)',
     value: '随心所欲'
   },
@@ -116,6 +175,20 @@ const strategyOptions: Array<{
   }
 ]
 
+const themeOptions: {
+  label: string;
+  value: string;
+}[] = [
+  {
+    label: '傀影',
+    value: 'Phantom'
+  },
+  {
+    label: '水月',
+    value: 'Mizuki'
+  }
+]
+
 const props = defineProps<{
   configurations: RogueConfiguration;
   taskIndex: number
@@ -129,47 +202,50 @@ function handleUpdateConfiguration (key: string, value: any) {
   updateTaskConfigurations(key, value, props.taskIndex)
 }
 
+function handleUpdateRogueTheme(value:string) {
+  updateTaskConfigurations('theme', value, props.taskIndex)
+  // 切主题, 检查一下分队
+  if (value === RogueTheme.Phantom) {
+    if (!phantomSquadOptions.find((item) => item.value === props.configurations.squad)) {
+      showMessage('当前分队不支持傀影主题, 已自动切换到指挥分队', { type: 'info', duration: 5000, closable: true })
+      handleUpdateConfiguration('squad', '指挥分队')
+    }
+  } else {
+    if (!mizukiSquadOptions.find((item) => item.value === props.configurations.squad)) {
+      showMessage('当前分队不支持水月主题, 已自动切换到指挥分队', { type: 'info', duration: 5000, closable: true })
+      handleUpdateConfiguration('squad', '指挥分队')
+    }
+  }
+}
+
 function handleUpdateStartsCount (value: number | null) {
   if (value === null) value = 999
   handleUpdateConfiguration('starts_count', value)
 }
 
-const allSkills: Ref<any> = ref()
-const loading = ref(false)
-
-onMounted(async () => {
-  loading.value = true
-
-  allSkills.value = (await gamedataApi.getAllSkills()) as any
-  Object.keys(allSkills.value).forEach((key) => {
-    allSkills.value[key].name = allSkills.value[key].levels[0].name
-    allSkills.value[key].image = getSkillImage(allSkills.value[key].name)
-  })
-  loading.value = false
-})
-
 </script>
 
 <template>
-  <div class="configuration-form rogue-configuration">
+  <NForm
+    :show-label="true"
+    size="small"
+    label-align="left"
+    label-placement="left"
+    :show-feedback="false"
+  >
     <NSpace vertical>
       <NFormItem
         label="主题"
-        :show-label="true"
-        size="small"
-        label-align="left"
-        label-placement="left"
-        :show-feedback="false"
       >
-        <NSelect />
+        <NSelect
+          :disabled="configurationDisabled.re"
+          :value="props.configurations.theme"
+          :options="themeOptions"
+          @update:value="(value) => handleUpdateRogueTheme(value)"
+        />
       </NFormItem>
       <NFormItem
         label="作战策略"
-        :show-label="true"
-        size="small"
-        label-align="left"
-        label-placement="left"
-        :show-feedback="false"
       >
         <NSelect
           :disabled="configurationDisabled.re"
@@ -180,26 +256,16 @@ onMounted(async () => {
       </NFormItem>
       <NFormItem
         label="开局分队"
-        :show-label="true"
-        size="small"
-        label-align="left"
-        label-placement="left"
-        :show-feedback="false"
       >
         <NSelect
           :disabled="configurationDisabled.re"
           :value="props.configurations.squad"
-          :options="squadOptions"
+          :options="props.configurations.theme === RogueTheme.Phantom ? phantomSquadOptions : mizukiSquadOptions"
           @update:value="(value) => handleUpdateConfiguration('squad', value)"
         />
       </NFormItem>
       <NFormItem
         label="开局职业"
-        :show-label="true"
-        size="small"
-        label-align="left"
-        label-placement="left"
-        :show-feedback="false"
       >
         <NSelect
           :disabled="configurationDisabled.re"
@@ -209,13 +275,20 @@ onMounted(async () => {
         />
       </NFormItem>
       <NFormItem
-        label="作战次数"
-        :show-label="true"
-        size="small"
-        label-align="left"
-        label-placement="left"
-        :show-feedback="false"
+        label="开局干员"
       >
+        <NTooltip trigger="hover">
+          <template #trigger>
+            <NInput
+              :disabled="configurationDisabled.re"
+              :value="props.configurations.core_char"
+              @update:value="(value) => handleUpdateConfiguration('core_char', value)"
+            />
+          </template>
+          仅支持单个干员中！文！名！。默认识别练度自动选择
+        </NTooltip>
+      </NFormItem>
+      <NFormItem label="作战次数">
         <NInputNumber
           :min="0"
           :max="999"
@@ -225,6 +298,29 @@ onMounted(async () => {
           @update:value="handleUpdateStartsCount"
         />
       </NFormItem>
+      <NFormItem>
+        <NCheckbox
+          :disabled="configurationDisabled.re"
+          :checked="props.configurations.use_support"
+          @update:checked="(value) => handleUpdateConfiguration('use_support', value)"
+        >
+          开局干员使用助战
+        </NCheckbox>
+      </NFormItem>
+      <NFormItem>
+        <NTooltip trigger="hover">
+          <template #trigger>
+            <NCheckbox
+              :disabled="configurationDisabled.re"
+              :checked="props.configurations.use_nonfriend_support"
+              @update:checked="(value) => handleUpdateConfiguration('use_nonfriend_support', value)"
+            >
+              允许使用非好友助战
+            </NCheckbox>
+          </template>
+          是否可以是非好友助战干员，仅在勾选'开局干员使用助战'时有效
+        </NTooltip>
+      </NFormItem>
     </NSpace>
-  </div>
+  </NForm>
 </template>
