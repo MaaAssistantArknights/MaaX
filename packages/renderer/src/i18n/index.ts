1b5eeebbcfc7f69e821b3d811a05f62b8e7e23ad
lzh <920136080@qq.com>
2023-05-26 15:08:12 +0800

    ✨ feat(i18n): 添加国际化的类型提示

--- packages/renderer/src/i18n/index.ts
+++ packages/renderer/src/i18n/index.ts
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-duplicates */
 import { zhCN as NZhCN, dateZhCN as NDateZhCN } from 'naive-ui'
 import { enUS as NEnUS, dateEnUS as NDateEnUS } from 'naive-ui'
-import { createI18n } from 'vue-i18n'
+import { createI18n, useI18n } from 'vue-i18n'
 
 import ZhCN from './zhCN.json'
 import EnUS from './enUS.json'
 import type { BooleanDisplayOption } from '@type/i18n/boolean'
+import type { I18nKey } from '@type/i18n/utils'
+import type { GetFnRemoveFirstParams } from '@type/utils'
 
 export const naiveUiLocale = {
   NZhCN,
@@ -24,6 +26,15 @@ export const i18n = createI18n({
   locale: 'EnUS',
   messages: appLocale,
 })
+
+/**
+ * 添加了键名检查的 `useI18n`
+ */
+export const useCustomI81n = () => {
+  const { t: _t, ...rest } = useI18n()
+  const t = (key: I18nKey, ...rest: GetFnRemoveFirstParams<typeof _t>) => _t(key, ...rest)
+  return { t, ...rest }
+}
 
 export const showBoolean = (value: boolean, option: BooleanDisplayOption = 'Yes/No'): string => {
   switch (option) {
