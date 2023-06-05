646f6cc9d0161b17fe4036e67e72a188c22202c6
lzh <920136080@qq.com>
2023-06-06 00:23:51 +0800

    🐞 fix(renderer/sidebars/Tool): 调整返回的路由

--- packages/renderer/src/layouts/sidebars/Tool.vue
+++ packages/renderer/src/layouts/sidebars/Tool.vue
@@ -77,7 +77,7 @@ const menuOptions: MenuOption[] = [
 
 <template>
   <div>
-    <NButton text style="font-size: 24px" @click="$router.back()">
+    <NButton text style="font-size: 24px" @click="$router.replace(`/task/${currentUuid}`)">
       <NIcon>
         <IconChevronLeft />
       </NIcon>
