58c08f66a2f2145a69b8b823b96b1204256d3fc7
bakashigure <bakashigure@hotmail.com>
2023-08-30 00:23:30 +0800

    feat(skip ci): 迁移至koffi

--- packages/main/coreLoader/callback.ts
+++ packages/main/coreLoader/callback.ts
@@ -1,12 +1,8 @@
 import logger from '@main/utils/logger'
-import ffi from '@tigerconnect/ffi-napi'
-import ref from '@tigerconnect/ref-napi'
 import type { AsstMsg } from '@type/task/callback'
+import type { AsstApiCallback } from './types'
 
-const callbackHandle = ffi.Callback(
-  'void',
-  ['int', 'string', ref.refType(ref.types.void)],
-  (code: AsstMsg, data: string, customArgs) => {
+export const callbackHandle: AsstApiCallback = (code, data, customArgs) => {
     logger.silly(code)
     logger.silly(data)
     globalThis.renderer.CoreLoader.callback({
@@ -14,6 +10,3 @@ const callbackHandle = ffi.Callback(
       data: JSON.parse(data),
     })
   }
-)
-
-export default callbackHandle
