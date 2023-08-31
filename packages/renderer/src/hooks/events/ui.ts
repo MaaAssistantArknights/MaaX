6535e00e20f26a91fd043b90919e1e853d4af5e4
liuyiyang <chingc.ccmail@gmail.com>
2023-08-31 13:44:48 +0800

    feat: Update name in task bar

--- packages/renderer/src/hooks/events/ui.ts
+++ packages/renderer/src/hooks/events/ui.ts
@@ -7,7 +7,7 @@ export default function useUiHooks(): void {
   window.renderer.WindowManager = {
     showMessage({ message, options }, event) {
       // ? 是否需要用string包裹防止xss
-      event.returnValue = window.$message.create(String(message), options)
+      // event.returnValue = window.$message.create(String(message), options)
     },
     loaded() {
       //   window.removeLoading()
